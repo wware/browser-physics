4b2cfde413d7dcd8a875adeba4e7656e0c622994
Will Ware <wware@veracode.com>
2016-12-28 23:09:55 -0500

    plod

--- graphics.js
+++ graphics.js
@@ -1,10 +1,10 @@
 // This assumes jquery has already been loaded.
 
 function getCanvas(parent, width, height) {
-    var canvasElement = $(
-        "<canvas width='" + width +
-        "px' height='" + height + "px'></canvas>"
-    ).appendTo(parent);
+    var canvasElement = $("<canvas>")
+        .attr('width', '' + width + 'px')
+        .attr('height', '' + height + 'px')
+        .appendTo(parent);
     var canvas = canvasElement.get(0).getContext("2d");
     canvas.center = Vector(width / 2, height / 2);
     return canvas;
